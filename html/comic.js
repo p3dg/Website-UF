d04ef20c55ad1218e0e3ac2cdbe88db8ca16be40
Peter Galvin <pgalvin@carboniteinc.com>
2015-04-23 10:41:49 -0400

    added new comics, jquery to apply change to last tag in comics button

---
+++ html/comic.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){$("#first").attr("href", "robolove.html");});
+$(document).ready(function(){$("#last").attr("href", "rating.html");});
+
+
+
---
+++ js/quotegenerator.js
@@ -0,0 +1,28 @@
+
+
+(function() {
+  "use strict";
+
+  var el = document.getElementById("quotes");
+
+  var quotes = [
+    "Stay Hungry. Stay Foolish. - Steve Jobs",
+    "Good Artists Copy, Great Artists Steal. - Pablo Picasso",
+    "Argue with idiots, and you become an idiot. - Paul Graham",
+    "Be yourself; everyone else is already taken. - Oscar Wilde",
+    "Simplicity is the ultimate sophistication. - Leonardo Da Vinci"
+  ];
+
+  function rand(xs) {
+    return xs.sort(function(){
+      return .5 - Math.random();
+    });
+  }
+
+  function quote(q) {
+    return q[0];
+  }
+  var quotemap = rand(quotes).join('');
+  el.innerHTML = quotemap[0];
+
+}());

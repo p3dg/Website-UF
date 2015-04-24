96c8b3e43b0084346eedef3a46274bc0535c10d1
Peter Galvin <pgalvin@carboniteinc.com>
2015-04-24 11:50:55 -0400

    many mods to files

--- html/comic.js
+++ html/comic.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){$("#first").attr("href", "robolove.html");});
-$(document).ready(function(){$("#last").attr("href", "rating.html");});
+$(document).ready(function(){$("#last").attr("href", "semanticbear.html");});
 
 
 

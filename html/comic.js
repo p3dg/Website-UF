5d41570a53bbd084514023a7985b2369dd615138
Peter Galvin <pgalvin@carboniteinc.com>
2015-05-13 21:02:39 -0400

    new comic, adding mobile ish stuff

--- html/comic.js
+++ html/comic.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){$("#first").attr("href", "robolove.html");});
-$(document).ready(function(){$("#last").attr("href", "semanticbear.html");});
+$(document).ready(function(){$("#last").attr("href", "thisislove.html");});
 
 
 
